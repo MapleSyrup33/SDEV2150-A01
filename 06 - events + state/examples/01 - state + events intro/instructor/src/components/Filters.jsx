a3de437503c97f29efaf3b36a96209e22fe3b4a3
oaao <oliver+github@abolish.golf>
2026-03-04 15:20:46 -0700

    rev [06 - state]: ex01 - hook up Filters searchbox to React state

--- 06 - events + state/examples/01 - state + events intro/instructor/src/components/Filters.jsx
+++ 06 - events + state/examples/01 - state + events intro/instructor/src/components/Filters.jsx
@@ -1,7 +1,14 @@
+// react hooks
+import { useState } from 'react';
+
+// my components
 import Card from './ui/Card';
 
 // src/components/Filters.jsx
 export default function Filters() {
+
+  const [searchTerm, setSearchTerm] = useState('some other value')
+
   return (
     <Card title="Filters">
       <div className="space-y-4 p-4">
@@ -15,6 +22,8 @@ export default function Filters() {
               type="text"
               placeholder="Try: tutoring, mental health, bursary"
               className="w-full rounded border border-gray-300 px-3 py-2 text-sm text-gray-900 placeholder-gray-400 focus:border-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-200"
+              onChange={(e) => {setSearchTerm(e.target.value)}}
+              value={searchTerm}
             />
           </div>
 
